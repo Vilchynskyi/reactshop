696572813f8f35e44ddd7bc00388db7f9cec1c1e
Vilya22 <roma.vilchynskyy@gmail.com>
2020-08-10 20:05:01 +0300

    OOP to products  added

--- src/apps/main/Products/ProductListItem.js
+++ src/apps/main/Products/ProductListItem.js
@@ -1,31 +1,41 @@
-import React from "react"
+import React, { Component } from "react"
 
 import PropTypes from 'prop-types';
 
 import "./ProductListItem.css"
 
-const ProductListItem = ({
-    name,
-    description = "No description",
-    type,
-    capacity,
-    price,
-    image = '/images/products/iphone.png',
-}) => {
-    return (
-        <div className="product-list-item">
-            <div className="product-image">
-                <img src={image} alt=""/>
-            </div> 
-            <div className="product-title">{name}</div>
-            <div className="product-desc">{description}</div>
-            <div className="product-features">Type: {type}</div>
-            <div className="product-features">Capacity: {capacity}Gb</div>
-            <div className="product-price">${price}</div>
-            <button className="btn-add-to-cart">Add to cart</button>
-        </div>
-    )
+
+class ProductListItem extends Component {
+    render() {
+        return (
+            <div className="product-list-item">
+                <div className="product-image">
+                    <img src={this.props.image} alt=""/>
+                </div> 
+                <div className="product-title">{this.props.name}</div>
+                <div className="product-desc">{this.props.description}</div>
+                <div className="product-features">Type: {this.props.type}</div>
+                <div className="product-features">Capacity: {this.props.capacity}Gb</div>
+                <div className="product-quantity">
+                    <button>-</button>
+                    <input type="text" defaultValue="1"/>
+                    <button>+</button>
+                </div>
+                <div className="product-price">${this.props.price}</div>
+                <button className="btn-add-to-cart">Add to cart</button>
+            </div>
+        )
+    }
 }
+
+// const ProductListItem = ({
+//     name,
+//     description = "No description",
+//     type,
+//     capacity,
+//     price,
+//     image = '/images/products/iphone.png',
+// }) => {}
 
 ProductListItem.propTypes = {
     name:PropTypes.string.isRequired,
