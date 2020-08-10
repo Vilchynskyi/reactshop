4170a033a3de138635ea427d7d6b23e6a565e4c1
Vilya22 <roma.vilchynskyy@gmail.com>
2020-08-10 20:13:06 +0300

    destructurisation added

--- src/apps/main/Products/ProductListItem.js
+++ src/apps/main/Products/ProductListItem.js
@@ -7,21 +7,29 @@ import "./ProductListItem.css"
 
 class ProductListItem extends Component {
     render() {
+        const {
+            name,
+            description = "No description",
+            type,
+            capacity,
+            price,
+            image = '/images/products/iphone.png',
+        } = this.props
         return (
             <div className="product-list-item">
                 <div className="product-image">
-                    <img src={this.props.image} alt=""/>
+                    <img src={image} alt=""/>
                 </div> 
-                <div className="product-title">{this.props.name}</div>
-                <div className="product-desc">{this.props.description}</div>
-                <div className="product-features">Type: {this.props.type}</div>
-                <div className="product-features">Capacity: {this.props.capacity}Gb</div>
+                <div className="product-title">{name}</div>
+                <div className="product-desc">{description}</div>
+                <div className="product-features">Type: {type}</div>
+                <div className="product-features">Capacity: {capacity}Gb</div>
                 <div className="product-quantity">
                     <button>-</button>
                     <input type="text" defaultValue="1"/>
                     <button>+</button>
                 </div>
-                <div className="product-price">${this.props.price}</div>
+                <div className="product-price">${price}</div>
                 <button className="btn-add-to-cart">Add to cart</button>
             </div>
         )
@@ -29,12 +37,7 @@ class ProductListItem extends Component {
 }
 
 // const ProductListItem = ({
-//     name,
-//     description = "No description",
-//     type,
-//     capacity,
-//     price,
-//     image = '/images/products/iphone.png',
+
 // }) => {}
 
 ProductListItem.propTypes = {
