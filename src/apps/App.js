9b899331738c244e726ac1c1dd81c6ea546945d9
Vilya22 <roma.vilchynskyy@gmail.com>
2020-08-26 20:28:06 +0300

    Method addToCart updated

--- src/apps/App.js
+++ src/apps/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import { has, update } from "lodash"
+// import { has, update } from "lodash"
 
 import '../common/style/reset.css'
 import '../common/style/base.css'
@@ -16,22 +16,23 @@ import productsData from "./Main/Products/productsData.js"
 class App extends Component {
 	
 	state = {
-		productsInCart: {}
+		productsInCart: {
+			// 1:6
+		}
 	}
 
 	addToCart = (productId, productCount) => {
-		console.log(productId)
-		if (has(this.state.productsInCart, `[${productId}]`)) {
-			const tempCart =	update(this.state.productsInCart, `[${productId}]`, function(count){
-				return count + productCount
-			});
-			this.setState({tempCart})
-		} else {
-			const tempCart = this.state.productsInCart[productId] = productCount
-			this.setState({
-				...this.state.productsInCart, ...tempCart
-			})
-		}
+		this.setState((prevState) => ({
+			// productsInCart: Object.assign(
+			// 	{},
+			// 	prevState.productsInCart,
+			// 	{[productId]: (prevState.productsInCart[productId] || 0) + productCount}
+			// )
+			productsInCart: {
+				...prevState.productsInCart,
+				[productId]: (prevState.productsInCart[productId] || 0) + productCount
+			}
+		}))
 	}
 	
 	render() {
